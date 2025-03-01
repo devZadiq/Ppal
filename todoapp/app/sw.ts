19c72ecb13f21afe7c9ece0ae6df42137a2581dc
Hazrat Mosaddique Ali <dev.zadiq@gmail.com>
2025-03-01 15:38:50 +0530

    Update sw.ts

--- todoapp/app/sw.ts
+++ todoapp/app/sw.ts
@@ -1,23 +1,17 @@
 /// <reference lib="webworker" />
 
-// This service worker can be customized!
-// See https://developers.google.com/web/tools/workbox/modules
-// for the list of available Workbox modules, or add any other
-// code you'd like.
+declare const self: ServiceWorkerGlobalScope; // Explicit type declaration
 
-// You can also remove this file if you'd prefer not to use a
-// service worker, and the Workbox build step will be skipped.
-
-const CACHE_NAME = "taskflow-cache-v1"
-const urlsToCache = ["/", "/manifest.json", "/icons/icon-192x192.png", "/icons/icon-512x512.png"]
+const CACHE_NAME = "taskflow-cache-v1";
+const urlsToCache = ["/", "/manifest.json", "/icons/icon-192x192.png", "/icons/icon-512x512.png"];
 
 // Install a service worker
 self.addEventListener("install", (event) => {
-  // Perform install steps
+  console.log("Service Worker installing");
   (event as ExtendableEvent).waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log("Opened cache");
-      return cache.addAll(urlsToCache); // Return the Promise from cache.addAll
+      return cache.addAll(urlsToCache);
     })
   );
   self.skipWaiting();
@@ -25,12 +19,15 @@ self.addEventListener("install", (event) => {
 
 // Cache and return requests
 self.addEventListener("fetch", (event) => {
+  console.log("Service Worker fetching", event.request.url);
   event.respondWith(
     caches.match(event.request).then((response) => {
       // Cache hit - return response
       if (response) {
+        console.log("Cache hit for", event.request.url);
         return response;
       }
+      console.log("Cache miss, fetching from network", event.request.url);
       return fetch(event.request).then((response) => {
         // Check if we received a valid response
         if (!response || response.status !== 200 || response.type !== "basic") {
@@ -41,6 +38,7 @@ self.addEventListener("fetch", (event) => {
         const responseToCache = response.clone();
 
         caches.open(CACHE_NAME).then((cache) => {
+          console.log("Caching", event.request.url);
           cache.put(event.request, responseToCache);
         });
 
@@ -52,12 +50,14 @@ self.addEventListener("fetch", (event) => {
 
 // Update a service worker
 self.addEventListener("activate", (event) => {
+  console.log("Service Worker activating");
   const cacheWhitelist = [CACHE_NAME];
   (event as ExtendableEvent).waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log("Deleting old cache", cacheName);
             return caches.delete(cacheName);
           }
           return null;
